3e62ff86169651fc26451043be7afeee9e023853
Joe Whitehead <34864012+M-Moon@users.noreply.github.com>
2023-04-11 08:09:36 +0100

    wowee

--- src/maze_solver/models/maze.py
+++ src/maze_solver/models/maze.py
@@ -1,2 +1,44 @@
 # maze.py
 
+from dataclasses import dataclass
+from functools import cached_property
+from typing import Iterator
+
+from .square import Square
+
+
+@dataclass(frozen=True)
+class Maze:
+    squares: tuple[Square, ...]
+
+    def __post__init(self) -> None:
+        validate_indices(self)
+        validate_rows_columns(self)
+
+    def __iter__(self) -> Iterator[Square]:
+        return iter(self.squares)
+
+    def __getitem__(self, index: int) -> Square:
+        return self.squares[index]
+
+    @cached_property
+    def width(self):
+        return max(square.column for square in self) + 1
+
+    @cached_property
+    def height(self):
+        return max(square.row for square in self) + 1
+
+
+def validate_indices(maze: Maze) -> None:
+    assert [square.index for square in maze] == list(
+        range(len(maze.squares))
+    ), "Wrong square.index"
+
+
+def validate_rows_columns(maze: Maze) -> None:
+    for y in range(maze.height):
+        for x in range(maze.width):
+            square = maze[y * maze.width + x]
+            assert square.row == y, "Wrong square.row"
+            assert square.column == x, "Wrong square.column"
